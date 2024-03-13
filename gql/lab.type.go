ae259a30b80d634b19fff6b5a5b81717f6eb36df
Ikhsan Haikal <47350060+ikhsanhaikal@users.noreply.github.com>
2024-03-13 13:23:40 +0700

    added resolvers function for lab queries
    1. get lecturers based on lab
    2. listing existing lab
    3. get a lab based on it's id

--- gql/lab.type.go
+++ gql/lab.type.go
@@ -1,7 +1,11 @@
 package gql
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/graphql-go/graphql"
+	"github.com/ikhsanhaikal/rate-my-lecturer-graphql-admin/be-app/sqlcdb"
 )
 
 func (builder TypeBuilder) LabType() *graphql.Object {
@@ -19,4 +23,53 @@ func (builder TypeBuilder) LabType() *graphql.Object {
 			},
 		},
 	})
+}
+
+func (r *Resolver) GetLecturersByLab(p graphql.ResolveParams) (interface{}, error) {
+	id, ok := p.Args["id"].(int)
+
+	if !ok {
+		return nil, errors.New("invalid pk")
+	}
+	queries := sqlcdb.New(r.DB)
+	members, err := queries.ListMembers(p.Context, int32(id))
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("lecturers_by_lab: %+v\n", members)
+
+	return members, nil
+}
+
+func (r *Resolver) ListLabs(p graphql.ResolveParams) (interface{}, error) {
+	queries := sqlcdb.New(r.DB)
+
+	labs, err := queries.ListLabs(p.Context)
+
+	fmt.Printf("labs: %+v\n", labs)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return labs, nil
+}
+func (r *Resolver) GetLabById(p graphql.ResolveParams) (interface{}, error) {
+	queries := sqlcdb.New(r.DB)
+
+	id, ok := p.Args["id"].(int)
+
+	if !ok {
+		return nil, errors.New("server error")
+	}
+
+	lab, err := queries.GetLab(p.Context, int32(id))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return lab, nil
 }
