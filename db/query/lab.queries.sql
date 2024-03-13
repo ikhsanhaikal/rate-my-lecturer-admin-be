3ce7bf9bb96e59121e9558b7a133bae505be705a
Ikhsan Haikal <47350060+ikhsanhaikal@users.noreply.github.com>
2024-03-13 12:59:01 +0700

    added query for listing lecturers basen on lab

--- db/query/lab.queries.sql
+++ db/query/lab.queries.sql
@@ -1,3 +1,11 @@
 -- name: GetLab :one
 SELECT * FROM labs
 WHERE id = ?;
+
+-- name: ListLabs :many
+SELECT * FROM labs;
+
+-- name: ListMembers :many
+SELECT * FROM lecturers l1 
+JOIN labs l2  ON l1.labId = l2.id
+WHERE l2.id = ?;
