d7eec158fe1d9a252d7d61019855efffa4190a5b
Ikhsan Haikal <47350060+ikhsanhaikal@users.noreply.github.com>
2024-03-17 23:09:33 +0700

    added 2 queries definisi create dan delete lab  untuk sqlc

--- db/query/lab.queries.sql
+++ db/query/lab.queries.sql
@@ -1,4 +1,4 @@
--- name: GetLab :one
+-- name: GetLabsByPk :one
 SELECT * FROM labs
 WHERE id = ?;
 
@@ -9,3 +9,10 @@ SELECT * FROM labs;
 SELECT * FROM lecturers l1 
 JOIN labs l2  ON l1.labId = l2.id
 WHERE l2.id = ?;
+
+-- name: CreateLab :execresult
+INSERT INTO labs (name, code, description) 
+VALUES (?, ?, ?);
+
+-- name: DeleteLab :execresult
+DELETE FROM labs WHERE labs.id = ?;  
