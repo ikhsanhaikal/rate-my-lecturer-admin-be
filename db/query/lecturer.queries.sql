f0939914d46e020476c2cea302d8dfde038ee03c
Ikhsan Haikal <47350060+ikhsanhaikal@users.noreply.github.com>
2024-03-17 23:00:42 +0700

    added  queries for update a lecturer

--- db/query/lecturer.queries.sql
+++ db/query/lecturer.queries.sql
@@ -1,15 +1,36 @@
--- name: CreateLecturer :execresult
+-- name: CreateLecturer :execlastid
 INSERT INTO lecturers (name, email, description, gender, labId) 
 			 VALUES (?, ?, ?, ?, ?);
 
--- name: DeleteLecturer :exec
+-- name: DeleteLecturersByPk :exec
 DELETE FROM lecturers
 WHERE id = ?;
 
--- name: ListLecturers :many
-SELECT * FROM lecturers;
+/*
+null restriction nggak bisa bro
+INSERT INTO lecturers(id, name, email, description, gender, labId)   
+VALUES (?, ?, ?, ?, ?, ?)  
+ON DUPLICATE KEY UPDATE 
+		name = VALUES(name),
+		email = VALUES(email),  
+		description = VALUES(description),
+		gender = VALUES(gender),
+		labId = VALUES(labId);*/
 
--- name: GetLecturerById :one
+-- name: UpdateLecturer :exec
+UPDATE lecturers  
+SET 
+    name = COALESCE(sqlc.narg(name) , name),
+    email = COALESCE(sqlc.narg(email), email),
+    description = COALESCE(sqlc.narg(description), description),
+    gender  = COALESCE(sqlc.narg(gender), gender),
+    labId  = COALESCE(sqlc.narg(labId), labId)
+WHERE id = sqlc.arg(id);
+
+-- name: ListLecturers :many
+SELECT *  FROM lecturers;
+
+-- name: GetLecturersByPk :one
 SELECT * FROM lecturers 
 WHERE lecturers.id = ?;
 
